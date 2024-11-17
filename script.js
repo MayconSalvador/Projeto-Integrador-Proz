e071394e6f9fb6826c2c2ee70a6491d9bebc4e33
noviva1 <noviva1@gmail.com>
2024-11-17 15:28:36 -0300

    estilização e validação do formulário

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
 
-const nomeInput = document.getElementById('nome');
-const emailInput = document.getElementById('email'); 
-let nomeIn = document.querySelector('input [for="nome]');  
-let nomePlaceholder= document.querySelector (placeholder="*Nome"); 
-
-//função mostrar popup de campo obrigatório
-
-function mostrarPopup (){
-nomeInput.classList.add("requerid-popup")
-}
-nomeInput.addEventListener("focus", mostrarPopup)
+// Validação do formulário
+form.addEventListener("submit",(event) => {
+        event.preventDefault();
+        checkForm();
+})
+nome.addEventListener("blur",() => { 
+    checkInputnome();})
+email.addEventListener("blur",() => { 
+    checkEmail();
+})
         
-function estilizarNomeValido(){
-    nomeInput.classList.add("correct");
-    nomeInput.classList.remove("error");
-    nomeInput.classList.remove("visible");
+function checkInputnome(){
+const nomeValue = nome.value;
+// Verificar se o nome está preenchido
+if (nome.value === "") {
+   errorInput(nome,"Por favor, preencha o seu nome!");
+ } else {
+    const formItem = nome.parentElement;
+    formItem.className ="form-group"
+ }
+ }
+ // Veerificar  email preenchido
+function checkEmail(){ 
+    const emailValue= email.value;
+if(emailValue === "" ) {
+    errorInput (email,"campo obrigatório")
+
+}else if (!isValidEmail(email.value)){
+errorInput(email,"preecha um email válido")
+}else {
+     const formItem =email.parentElement;
+     formItem.className ="form-group"
+}
 }
 
-function estilizarUserNameInvalido(){
-    nomeInput.classList.add("error");
-    nomeInput.classList.remove("correct");
-    nomeInput.classList.add("visible");
-   nomeInput.innerText="mínimo 3 caracteres";
+//Função para validar o email
+function isValidEmail(email) {
+    const emailRegex = new RegExp(
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    );
+if (emailRegex.test(email)) {
+        return true; 
+ }
+  return false;
+  }
+  
+
+     
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMenssage= formItem.querySelector("a");
+
+    textMenssage.innerText = message;
+    formItem.className= "form-group error"
 }
+
+function checkForm() {
+    checkInputnome();
+    checkEmail()
+    const formItems =form.querySelectorAll(".form-group")
+    const isValid =[...formItems].every((item) => {
+        return item.className === "form-group"
+    });
+if (isValid) {
+    alert("INSCRIÇÃO REALIZADA COM SUCESSO!")
+}}
